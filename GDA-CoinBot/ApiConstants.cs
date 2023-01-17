bd3cac22809f451bbccb4dc9b98144eaecfd9cce
vitaly <shcherbinovv@gmai.com>
2023-01-17 12:05:00 +0600

    code refactor

--- GDA-CoinBot/ApiConstants.cs
+++ GDA-CoinBot/ApiConstants.cs
@@ -1,5 +1,5 @@
 public static class ApiConstants
 {
-    public const string BOT_API = "5803769896:AAH6hR3g6W_jHK6_7_73IcjdPnhmOHb2zww";
-    public const string COIN_MARKET_API = "a68013e3-48b6-45cb-8947-64e2e2bf1440";
+    public const string BOT_API = "ВАШ КЛЮЧ";
+    public const string COIN_MARKET_API = "ВАШ КЛЮЧ";
 }
